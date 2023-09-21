f1abbc2762e473e03f86a1a69488ee1e19c65a76
Octavio Martinez <omartinez1618@gmail.com>
2023-09-21 03:38:07 -0400

    custom user

--- src/app/form/layout.tsx
+++ src/app/form/layout.tsx
@@ -7,16 +7,18 @@ export default function Form({
     children: React.ReactNode
 }) {
     const dependencie = cookies().get(process.env.COOKIE_NAME_TARGET as string)?.value
+    const user = cookies().get(process.env.COOKIE_NAME_USER as string)?.value
     async function search(data:FormData) {
         "use server";
         const response = await fetch( process.env.LOCALHOST + "/api/search", {
         method:'post',
         body:JSON.stringify({
-            user:"zenx5",
+            user:data.get("user"),
             dep: data.get("dependencie")
         })
         })
         const result = await response.json()
+        cookies().set(process.env.COOKIE_NAME_USER as string, data.get("user") as string)
         cookies().set(process.env.COOKIE_NAME_TARGET as string, data.get("dependencie") as string)
         cookies().set(process.env.COOKIE_NAME_RESULT as string, JSON.stringify(result))
         redirect("/form")
@@ -25,8 +27,15 @@ export default function Form({
 
     return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-10 p-24">
-      <form className="w-1/3 flex flex-row" action={search}>
-        <input type="text" className="py-2 px-4 rounded w-10/12" placeholder="Dependencie" name="dependencie" defaultValue={dependencie ?? ""}/>
+      <form className="w-1/2 flex flex-row" action={search}>
+        <span className="w-4/12 border-r">
+          <input type="text" className="py-2 px-4 m-0 w-full h-full" placeholder="Usario" name="user" defaultValue={user ?? ""}/>
+          <small className="italic ml-5 text-slate-500">Usuario de githbu</small>
+        </span>
+        <span className="w-4/12 border-r">
+          <input type="text" className="py-2 px-4 m-0 w-full h-full" placeholder="Dependencia" name="dependencie" defaultValue={dependencie ?? ""}/>
+          <small className="italic ml-5 text-slate-500">Dependencia que desea buscar</small>
+        </span>
         <button className="w-2/12 bg-green-500 text-white">Buscar</button>
       </form>
       <div className="">
