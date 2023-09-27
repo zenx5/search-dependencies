3a786b6bae842fec39d553e62669df50da0a7e04
Octavio Martinez <omartinez1618@gmail.com>
2023-09-27 10:45:23 -0400

    add host de las imagenes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
     experimental:{
         serverActions: true
+    },
+    images: {
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'avatars.githubusercontent.com',
+                port:'',
+                pathname:'/u/**'
+            }
+        ]
     }
 }
 
