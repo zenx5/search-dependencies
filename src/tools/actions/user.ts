70913d573734598569d48022b9d44dca580fff79
Octavio Martinez <omartinez1618@gmail.com>
2023-10-17 21:20:06 -0400

    getUser coookies

--- src/tools/actions/user.ts
+++ src/tools/actions/user.ts
@@ -6,6 +6,11 @@ export const setUser = ( user:any ) => {
     cookies().set(process.env.COOKIE_NAME_USER as string, JSON.stringify( user ) )
 }
 
+export const getUser = () => {
+    const user = cookies().get(process.env.COOKIEN_NAME_USER as string)
+    return user?.value ? JSON.parse( user.value ) : false
+}
+
 export const actionUser = (form:FormData) => {
     const response = form.get("response_is")
     const user = form.get("user")
