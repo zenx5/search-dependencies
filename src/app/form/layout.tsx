b0b482109b69886cb5eef58e8055eeb3d8e1590a
Octavio Martinez <omartinez1618@gmail.com>
2023-10-17 20:52:49 -0400

    centrar el 'buscando' y ajustar distancia del header

--- src/app/form/layout.tsx
+++ src/app/form/layout.tsx
@@ -43,7 +43,7 @@ export default function Form({
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-10 absolute  top-3 right-3">
         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
       </svg>
-      <form className="container flex md:flex-row flex-col gap-5 md:gap-0 justify-center" action={search}>
+      <form className="mt-20 container flex md:flex-row flex-col gap-5 md:gap-0 justify-center" action={search}>
         <span className="w-full md:w-4/12 lg:w-64 h-fit group">
           <input type="text" className="md:border-r text-center md:text-left py-2 px-4 m-0 w-full h-full rounded-xl md:rounded-l-xl md:rounded-r-none shadow-lg outline-none focus:bg-green-100 focus:font-semibold" placeholder="Usuario" name="user" defaultValue={user ?? ""}/>
           <small className="italic text-slate-500 group-hover:text-slate-900 text-center block w-full mt-1">Usuario de github</small>
