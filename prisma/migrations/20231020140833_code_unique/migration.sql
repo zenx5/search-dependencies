51934ba45ca88ed848c1aaf82f2563d3c0f5c30a
Octavio Martinez <omartinez1618@gmail.com>
2023-11-01 13:26:59 -0400

    remove prisma

--- prisma/migrations/20231020140833_code_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[code]` on the table `Extensions` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Extensions_code_key" ON "Extensions"("code");
